dcc39955349652791b824d9f4e426ba40294770a
Mshrimp <chiyuan.ma@outlook.com>
2017-03-15 19:33:34 +0800

    Pin

--- hal/pin.h
+++ hal/pin.h
@@ -7,7 +7,13 @@
 
 
 typedef	struct	{
-	void (*SetDirection)(void *pin, uint8 InOut);
+#define	PIN_DIR_INPUT	0x00
+#define	PIN_DIR_OUTPUT	0x01
+	void (*SetDirection)(void *PinInfo, uint8 InOut);
+#define	PIN_OUTPUT_LOW	0x00
+#define	PIN_OUTPUT_HIGH	0x01
+	void (*SetExport)(void *PinInfo, uint8	HighLow);
+	void (*GetPinValue)(void *PinInfo);
 
 
 }ClassPin;
