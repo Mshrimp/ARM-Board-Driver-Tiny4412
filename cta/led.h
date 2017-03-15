41879578f4ac8b3e2f100092f0be2818658838df
Mshrimp <chiyuan.ma@outlook.com>
2017-03-15 19:08:22 +0800

    head add

---
+++ cta/led.h
@@ -0,0 +1,15 @@
+#ifndef	__LED_H__
+#define	__LED_H__
+
+
+
+#include <../head.h>
+
+
+#define	LED_CON	GPM4CON_ADDR
+#define	LED_DAT	GPM4DAT_ADDR
+
+
+
+#endif	//__HEAD_H__
+
---
+++ head.h
@@ -0,0 +1,13 @@
+#ifndef	__HEAD_H__
+#define	__HEAD_H__
+
+
+#include <stdio.h>
+
+
+#include <hal/hal.h>
+
+
+
+#endif	//__HEAD_H__
+
