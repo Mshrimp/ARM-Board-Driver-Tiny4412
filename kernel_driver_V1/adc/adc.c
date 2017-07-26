227073f50efee12f4d042f6bb1fe6edb3162ed97
Mshrimp <chiyuan.ma@outlook.com>
2017-07-26 18:42:22 +0800

    Add adc debug

---
+++ kernel_driver_V1/adc/adc.c
@@ -0,0 +1,244 @@
+#include <linux/module.h>
+#include <linux/fs.h>
+
+#include "../include/board.h"
+#include "../include/head.h"
+#include "common.h"
+#include "adc.h"
+
+
+static volatile unsigned int *adc_cfg_p = NULL;
+static volatile unsigned int *adc_con_p = NULL;
+static volatile unsigned int *adc_dat_p = NULL;
+static volatile unsigned int *adc_mux_p = NULL;
+
+
+/////////////////////////////////////////////////////////////////////裸板驱动
+
+int adc_config(void)
+{
+	set_bit_val(adc_cfg_p, 16, ADC_GENERAL);
+	set_bit_val(adc_con_p, 16, ADCCON_12BIT_CONV);
+	set_bit_val(adc_con_p, 14, ADCCON_CONV_PRESC_ENABLE);
+	set_nbits_val(adc_con_p, 6, 8, 19);
+	return 0;
+}
+
+int is_adc_conv_end(void)
+{
+	return get_bit_val(adc_con_p, 15);
+}
+
+void adc_conv_read_enable(void)
+{
+	set_bit_val(adc_con_p, 1, ADCCON_CONV_READ_START_ENABLE);
+}
+
+void adc_conv_read_disable(void)
+{
+	set_bit_val(adc_con_p, 1, ADCCON_CONV_READ_START_DISABLE);
+}
+
+void adc_conv_start_enable(void)
+{
+	set_bit_val(adc_con_p, 0, ADCCON_CONV_START_ENABLE);
+}
+
+unsigned int adc_get_data(void)
+{
+	return get_nbits_val(adc_dat_p, 0, ADCDAT_12BIT);
+}
+
+void adc_set_channel(int num)
+{
+	set_nbits_val(adc_mux_p, 0, ADCMUX_AIN_BIT, num);
+}
+
+int adc_init(void)
+{
+	int ret = -1;
+
+	printk("ADC: Init pin\n");
+	ret = adc_config();
+	if (ret < 0)
+	{
+		printk("ADC: init, adc_config failed\n");
+		return -1;
+	}
+
+	adc_set_channel(0);
+	printk("ADC: set channel 0\n");
+
+	return 0;
+}
+
+void adc_conv(void)
+{
+	int i = 0;
+	int data = 0;
+
+	adc_conv_start_enable();
+	printk("ADC: start conv\n");
+
+	while(i < 10)
+	{
+		if (is_adc_conv_end())
+		{
+			printk("ADC: conv end\n");
+			data = adc_get_data();
+			printk("ADC: get data = %d, i = %d\n", data, i);
+			i++;
+			msleep(1000);
+			adc_conv_start_enable();
+			printk("ADC: start conv, i = %d\n", i);
+		}
+	}
+}
+
+////////////////////////////////////////////////////////////////////////////字符设备框架
+
+long adc_test_ioctl (struct file *filp, unsigned int cmd, unsigned long arg)
+{
+	printk("Driver: ADC test ioctl!\n");
+
+/*
+ *    if (_IOC_TYPE(cmd) != ADC_IOC_TYPE)
+ *    {
+ *        printk("Driver: ADC cmd type error!\n");
+ *        return	-EINVAL;
+ *    }
+ *
+ *    if (_IOC_NR(cmd) > ADC_IOC_MAX_NR)
+ *    {
+ *        printk("Driver: ADC cmd error, No such cmd!\n");
+ *        return	-EINVAL;
+ *    }
+ */
+
+/*
+ *    if (_IOC_DIR(cmd) & _IOC_READ) {
+ *        ret = access_ok(VERIFY_WRITE, (void *)args, _IOC_SIZE(cmd));
+ *    } else {
+ *        ret = access_ok(VERIFY_READ, (void *)args, _IOC_SIZE(cmd));
+ *    }
+ */
+
+/*
+ *    switch(cmd)
+ *    {
+ *        case 0:
+ *            break;
+ *
+ *        default:
+ *            printk("Driver: ADC cmd error");
+ *            return	-EINVAL;
+ *    }
+ */
+	return 0;
+}
+
+ssize_t adc_test_write (struct file *filp, const char __user *buf, size_t size, loff_t *offset)
+{
+	printk("adc test write!\n");
+	return size;
+}
+
+ssize_t adc_test_read (struct file *filp, char __user *buf, size_t size, loff_t *offset)
+{
+	printk("adc test read!\n");
+	return size;
+}
+
+int adc_test_open (struct inode *inodep, struct file *filp)
+{
+	printk("adc test open!\n");
+	return 0;
+}
+
+int adc_test_close (struct inode *inodep, struct file *filp)
+{
+	printk("adc test close!\n");
+	return 0;
+}
+
+/////////////////////////////////////////////////////////////模块
+struct file_operations fops = {
+	.owner = THIS_MODULE,
+	.open = adc_test_open,
+	.release = adc_test_close,
+	.read = adc_test_read,
+	.write = adc_test_write,
+	.unlocked_ioctl = adc_test_ioctl,
+};
+
+int major = 0;
+
+static int adc_test_init(void)
+{
+	int ret = -1;
+	printk("Hello, adc driver chrdev register test begin!\n");
+
+	adc_con_p = ((volatile unsigned int *)ioremap((ADCCON_ADDR), 32));
+	ERRP_K(adc_con_p == NULL, "ADC", "adc_con_p ioremap", goto ERR_ioremap1);
+
+	adc_dat_p = ((volatile unsigned int *)ioremap((ADCDAT_ADDR), 32));
+	ERRP_K(adc_dat_p == NULL, "ADC", "adc_dat_p ioremap", goto ERR_ioremap2);
+
+	adc_mux_p = ((volatile unsigned int *)ioremap((ADCMUX_ADDR), 32));
+	ERRP_K(adc_mux_p == NULL, "ADC", "adc_mux_p ioremap", goto ERR_ioremap3);
+
+	adc_cfg_p = ((volatile unsigned int *)ioremap((ADCMUX_ADDR), 32));
+	ERRP_K(adc_cfg_p == NULL, "ADC", "adc_cfg_p ioremap", goto ERR_ioremap4);
+
+	printk("ADC: Init adc\n");
+	ret = adc_init();
+	ERRP_K(ret < 0, "ADC", "adc_init", goto ERR_init);
+
+	adc_conv();
+
+/*
+ *    ret = register_chrdev(major, ADC_DEV_NAME, &fops);
+ *    ERRP_K(ret < 0, "ADC", "register_chrdev", goto ERR_dev_register);
+ *
+ *    if (major == 0) {
+ *        major = ret;
+ *        printk("major = %d\n", major);
+ *    } else {
+ *        printk("major = %d\n", major);
+ *    }
+ */
+
+
+	return 0;
+
+ERR_init:
+	iounmap(adc_cfg_p);
+ERR_ioremap4:
+	iounmap(adc_mux_p);
+ERR_ioremap3:
+	iounmap(adc_dat_p);
+ERR_ioremap2:
+	iounmap(adc_con_p);
+ERR_ioremap1:
+	return -1;
+}
+
+static void adc_test_exit(void)
+{
+	printk("Goodbye, adc test over!\n");
+	iounmap(adc_con_p);
+	iounmap(adc_dat_p);
+	iounmap(adc_mux_p);
+	iounmap(adc_cfg_p);
+	//unregister_chrdev(major, ADC_DEV_NAME);
+}
+
+
+module_init(adc_test_init);
+module_exit(adc_test_exit);
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Mshrimp");
+MODULE_DESCRIPTION("Test adc");
+MODULE_VERSION("V0.1");
+
---
+++ kernel_driver_V1/adc/adc.h
@@ -0,0 +1,45 @@
+#ifndef	__ADC_H__
+#define	__ADC_H__
+
+
+#define	ADC_BASE		0x126C0000
+
+#define	ADCCON_OFFSET	0x0000
+#define	ADCDAT_OFFSET	0x000C
+#define	ADCMUX_OFFSET	0x001C
+
+
+#define	ADCCON_ADDR		(ADC_BASE + ADCCON_OFFSET)
+#define	ADCCON_10BIT_CONV		0x0	// [16]
+#define	ADCCON_12BIT_CONV		0x1
+#define	ADCCON_CONV_PROC		0x0	// [15]
+#define	ADCCON_CONV_END			0x1
+#define	ADCCON_CONV_PRESC_DISABLE	0x0	// [14]
+#define	ADCCON_CONV_PRESC_ENABLE	0x1
+#define	ADCCON_CONV_PRESC_DATA_MASK		(((0x1<<8)-1)<<6)	// [13:6]
+#define	ADCCON_CONV_READ_START_DISABLE	0	// [1]
+#define	ADCCON_CONV_READ_START_ENABLE	1
+#define	ADCCON_CONV_START_ENABLE		1	// [0]
+
+#define	ADCDAT_ADDR		(ADC_BASE + ADCDAT_OFFSET)
+#define	ADCDAT_DATA_MASK		0xFFF	// [11:0]
+#define	ADCDAT_10BIT			10
+#define	ADCDAT_12BIT			12
+
+
+
+#define	ADCMUX_ADDR		(ADC_BASE + ADCMUX_OFFSET)
+#define	ADCMUX_AIN0		0x0	// AIN0
+#define	ADCMUX_AIN1		0x1	// AIN0
+#define	ADCMUX_AIN2		0x2	// AIN0
+#define	ADCMUX_AIN3		0x3	// AIN0
+#define	ADCMUX_AIN_BIT	4	// [3:0]
+
+
+#define	ADC_CFG_ADDR	0x10010118
+#define	ADC_GENERAL		0x0	// [16]
+#define	ADC_MTCADC_ISP	0x1
+
+
+
+#endif	/* __ADC_H__ */
