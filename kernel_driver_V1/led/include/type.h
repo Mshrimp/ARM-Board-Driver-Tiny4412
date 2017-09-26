c2f174cfaf79125a2bb5180143fb11af10f57080
Mshrimp <chiyuan.ma@outlook.com>
2017-09-26 15:46:42 +0800

    Type clean

---
+++ kernel_driver_V1/led/include/type.h
@@ -0,0 +1,19 @@
+#ifndef	__TYPE_H__
+#define	__TYPE_H__
+
+
+typedef	char					s8int;
+typedef	unsigned char			u8int;
+
+typedef	short int				s16int;
+typedef	unsigned short int		u16int;
+
+typedef	int						s32int;
+typedef	unsigned int			u32int;
+
+typedef	long long int			s64int;
+typedef	unsigned long long int	u64int;
+
+
+#endif
+
