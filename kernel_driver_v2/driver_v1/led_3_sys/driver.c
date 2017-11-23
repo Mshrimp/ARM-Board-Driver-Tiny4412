15745efb866be70d9e269341a59774ce3de64c69
Mr.kevin <mayuanye1995@gmail.com>
2017-11-23 22:08:19 +0800

    Add led system api, gpio_request

---
+++ kernel_driver_v2/driver_v1/led_3_sys/driver.c
@@ -0,0 +1,136 @@
+#include <linux/module.h>
+#include <linux/fs.h>
+#include <linux/device.h>
+
+#include <linux/gpio.h>
+#include <mach/gpio.h>
+#include <plat/gpio-cfg.h>
+
+#include <asm/io.h>
+#include "common.h"
+
+#include "led.h"
+
+#define DEV_NAME	"test_driver"
+
+volatile unsigned long *led_con_p = NULL;
+volatile unsigned long *led_dat_p = NULL;
+
+
+static int led_gpios[] = {
+	EXYNOS4X12_GPM4(0),
+	EXYNOS4X12_GPM4(1),
+	EXYNOS4X12_GPM4(2),
+	EXYNOS4X12_GPM4(3),
+};
+
+#define LED_NUM	ARRAY_SIZE(led_gpios)
+
+////////////////////////////////////////////////////////////////////////////字符设备框架
+
+long driver_test_ioctl (struct file *filp, unsigned int cmd, unsigned long arg)
+{
+	printk("Driver: test ioctl!\n");
+
+	return 0;
+}
+
+ssize_t driver_test_write (struct file *filp, const char __user *buf, size_t size, loff_t *offset)
+{
+	printk("Driver: test write!\n");
+	return size;
+}
+
+ssize_t driver_test_read (struct file *filp, char __user *buf, size_t size, loff_t *offset)
+{
+	printk("Driver: test read!\n");
+	return size;
+}
+
+int driver_test_open (struct inode *inodep, struct file *filp)
+{
+	printk("Driver: test open!\n");
+
+	return 0;
+}
+
+int driver_test_close (struct inode *inodep, struct file *filp)
+{
+	printk("Driver: test close!\n");
+	return 0;
+}
+
+/////////////////////////////////////////////////////////////模块
+struct file_operations fops = {
+	.owner = THIS_MODULE,
+	.open = driver_test_open,
+	.release = driver_test_close,
+	.read = driver_test_read,
+	.write = driver_test_write,
+	.unlocked_ioctl = driver_test_ioctl,
+};
+
+int major = 0;
+struct class *driver_class;
+struct device *driver_class_device;
+
+static int driver_test_init(void)
+{
+	int i = 0;
+	int ret = -1;
+
+	printk("Hello, driver chrdev register test begin!\n");
+
+	major = register_chrdev(major, DEV_NAME, &fops);
+	ERRP_K(major < 0, "Driver", "register_chrdev", goto ERR_dev_register);
+
+	driver_class = class_create(THIS_MODULE, "driver_class");
+	ERRP_K(driver_class == NULL, "Driver", "class_create", goto ERR_class_create);
+
+	driver_class_device = device_create(driver_class, NULL, MKDEV(major, 0), NULL, "driver_class_device");
+	ERRP_K(driver_class_device == NULL, "Driver", "class_device_create", goto ERR_class_device_create);
+
+	printk("major = %d\n", major);
+	
+	for (i = 0; i < LED_NUM; i++) {
+		ret = gpio_request(led_gpios[i], "led");
+		if (ret) {
+			printk("%s: gpio_request gpio[%d] failed, ret = %d\n", __func__, i, ret);
+		}
+	}
+
+	s3c_gpio_cfgpin(led_gpios[0], S3C_GPIO_OUTPUT);
+	gpio_set_value(led_gpios[0], 1);
+
+	return 0;
+ERR_class_device_create:
+	class_destroy(driver_class);
+ERR_class_create:
+	unregister_chrdev(major, DEV_NAME);
+ERR_dev_register:
+	return -1;
+}
+
+static void driver_test_exit(void)
+{
+	int i = 0;
+	printk("Goodbye, test over!\n");
+
+	for (i = 0; i < LED_NUM; i++) {
+		gpio_free(led_gpios[i]);
+	}
+	//device_unregister(driver_class_device);
+	device_destroy(driver_class, MKDEV(major, 0));
+	class_destroy(driver_class);
+	unregister_chrdev(major, DEV_NAME);
+}
+
+
+module_init(driver_test_init);
+module_exit(driver_test_exit);
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Mshrimp");
+MODULE_DESCRIPTION("Test driver");
+MODULE_VERSION("V0.1");
+
