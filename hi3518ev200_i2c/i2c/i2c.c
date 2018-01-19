babe57036bf3c8595219598a6624a327fd1b10a5
Chiyuan.Ma <chiyuan.ma@netview.com>
2018-01-19 10:31:12 +0800

    Add hi3518ev200 i2c gpio

---
+++ hi3518ev200_i2c/i2c/i2c.c
@@ -0,0 +1,8 @@
+
+int i2c_init(int nr, unsigned long int slave_addr)
+{
+
+
+	return 0;
+}
+
---
+++ hi3518ev200_i2c/i2c/i2c.h
@@ -0,0 +1,16 @@
+#ifndef	__I2C_H__
+#define	__I2C_H__
+
+int i2c_init(void);
+
+int i2c_start(void);
+
+int i2c_stop(void);
+
+int i2c_ack(u8 ack_status);
+
+int i2c_write_byte(u8 data);
+
+int i2c_read_byte(u8 *data);
+
+#endif	/* __I2C_H__ */
